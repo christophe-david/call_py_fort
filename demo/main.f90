5d1fa9aa9440e603bc1a25dcde0dd55a45801724
Christophe DAVID <chrdav81@free.fr>
2021-07-05 12:09:48 +0200

    Completed a bit the demo.

--- demo/main.f90
+++ demo/main.f90
@@ -6,6 +6,8 @@ real(8) :: a(10)
 a = 1.0
 call set_state("a", a)
 call call_function("builtins", "print")
+call call_function("my_module", "my_function")
+call call_function("builtins", "print")
 ! read any changes from "a" back into a.
 call get_state("a", a)
 
---
+++ demo/my_module.py
@@ -0,0 +1,3 @@
+def my_function(STATE):
+    print(STATE["a"])
+    STATE["a"][:] = 50.
